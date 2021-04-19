5deceb38fa50466d6bdf58d2ac17c5fd50a77df3
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-04-18 20:08:25 -0500

    added notes using app

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
+// require db.json to eliminate need for fs.readFile
+const db = require('./db/db.json')
 const {v4 : uuidv4} = require('uuid');
 
 // Sets up the Express App
@@ -19,43 +22,51 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 //API routes
-// GET: /api/notes to fs.readFile db.json and return all saved notes as JSON (res.json)
-app.get('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) throw err
-    console.log(`FILE READ >>>>`, data)
-    res.json(data);
-  })
-})
+// GET: /api/notes to res.json required db.json file 'db'
+app.get('/api/notes', (req, res) => res.json(db));
 
 // POST: /api/notes to create newNote and saveNote
 app.post('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.log(`FAILED TO READ >>>>`, err)
-      return
-    }
-    // parse data into JSON object
-    let savedNotes = JSON.parse(data)
-    // newNote object because need to set id as obj.param
-    let newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uuidv4(),
-    }
-    // push newNote object to savedNotes
-    savedNotes.push(newNote)
-    console.log(savedNotes);
-    // update json to db.json > don't forget to stringify JSON and take out id
-    fs.writeFile('db/db.json',JSON.stringify(savedNotes), (err) => {
+    let newNote = req.body;
+    let dbPath = path.join(__dirname, './db/db.json')
+    // set unique id
+    newNote.id = uuidv4();
+    // push newNote object to req data
+    db.push(newNote)
+    console.log(db);
+    // update json to db.json > don't forget to stringify
+    fs.writeFile(dbPath, JSON.stringify(db), (err) => {
       err ? console.error(`FAILED TO WRITE >>>>`, err) : console.log(`New note saved!`)
     });
     res.json(newNote);
-  });
 })
 
 
-// DELETE deleteNote?
+// DELETE note using id of notes, .splice at index 1
+app.delete('/api/notes/:id', (req, res) => {
+  let noteId = req.params.id;
+  let dbPath = path.join(__dirname, './db/db.json')
+  // for loop to search through array, match, then splice
+  for (let i = 0; i < db.length; i++ ) {
+    if (db[i].id === noteId) {
+      db.splice(i, 1);
+      break;
+    }
+  }
+
+  // .forEach to replace for loop
+  // db.forEach((note, i) => {
+  //   if (db[i].id === noteId) {
+  //     db.splice(i, 1);
+  //   }
+  // })
+
+  // update json to db.json > don't forget to stringify JSON
+  fs.writeFile(dbPath, JSON.stringify(db), (err) => {
+    err ? console.error('FAILED TO DELETE NOTES >>>>', err) : console.log(`Delete sucessful!`)
+  });
+  res.json(db)
+})
 
 // Starts server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
